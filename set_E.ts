199498d2d0c81b465dab845a1cbffed49de1dd18
blues74 <blues74@mail.ru>
2022-01-15 22:40:12 +0300

    set E

--- set_E.ts
+++ set_E.ts
@@ -65,18 +65,18 @@ const enterSandmen = `
   </a>
 
 <pre style="font-family: monospace; margin: .5rem 0 0;">
-<span style="color: lightgray;">   му  МоКуЛу</span> <a data-note-line="b120 му-100 мо-100 ку-50 лу-100">PLAY</a>
+<span style="color: lightgray;">   му  МоКуЛу</span> <a data-note-line="b110 му-100 мо-100 ку-50 лу-100">PLAY</a>
 5: --2/7----
 6: 0-----6-5
-<span style="color: lightgray;">   суМу ВуМу ВуСуВу</span> <a data-note-line="b120 су-100 му-50 ву-50 му-50 ву-50 су-50 ву-50">PLAY</a>
+<span style="color: lightgray;">   суМу ВуМу ВуСуВу</span> <a data-note-line="b110 су-100 му-50 ву-50 му-50 ву-50 су-50 ву-50">PLAY</a>
 6: 3-0--2-0--2-3-2
-<span style="color: lightgray;">   муМуМуМуМуМуМуФу</span> <a data-note-line="b120 му-50 му-50 му-50 му-50 му-50 му-50 му-50 фу-50">PLAY</a>
+<span style="color: lightgray;">   муМуМуМуМуМуМуФу</span> <a data-note-line="b110 му-50 му-50 му-50 му-50 му-50 му-50 му-50 фу-50">PLAY</a>
 6: 0-0-0-0-0-0-0-1
 </pre>
 
 <br/>
 Переход к <strong>И вновь продолжается бой</strong>
-<a data-note-line="b120 му-100 су-50 ку-50 то-50 мо-50 со-50 ко-50  бу-100 ро-50 фо-50 зо-50 ра-50">PLAY</a>
+<a data-note-line="b110 му-100 су-50 ку-50 то-50 мо-50 со-50 ко-50  бу-100 ро-50 фо-50 зо-50 ра-50">PLAY</a>
 
 <pre style="font-family: monospace; margin: .5rem 0 0;">
 <span style="color: lightgray;">   муСуКуТоМоСоКо  буРоФоЗоРа</span>
@@ -110,9 +110,9 @@ const peterGunn = `
   </a>
 
 <pre style="font-family: monospace; margin: .5rem 0 0;">
-<span style="color: lightgray;">   муФуФуФу</span> <a data-note-line="b120 му-50 фу-50 фу-50 фу-50">PLAY</a>
+<span style="color: lightgray;">   муФуФуФу</span> <a data-note-line="b110 му-50 фу-50 фу-50 фу-50">PLAY</a>
 6: 0/1-1-1
-<span style="color: lightgray;">   муМу ВуМу СуМу ЛуЗу</span> <a data-note-line="b120 му-50 му-50 ву-50 му-50 су-50 му-50 лу-50 зу-50">PLAY</a>
+<span style="color: lightgray;">   муМу ВуМу СуМу ЛуЗу</span> <a data-note-line="b110 му-50 му-50 ву-50 му-50 су-50 му-50 лу-50 зу-50">PLAY</a>
 6: 0-0--2-0--3-0--5-4
 </pre>
 
@@ -135,24 +135,23 @@ const parts = `
 @sn:     2       4   :
 @bd: 1       3       :
 --------------------------------------------------------------------------------
-<pg_оХаОО_ritm$>
-
-$bass: му-50 фу-50 фу-50 фу-50 му-50 фу-50 фу-50 фу-50
---------------------------------------------------------------------------------
-<pg_00ЦаЦуЩаЩуПаХу_ritm$>
-$organ: му-50 му-50 ву-50 му-50 су-50 му-50 лу-50 зу-50
---------------------------------------------------------------------------------
-<pgToEs$>
-$organ: му-50 му-50  ву-50 му-50  су-50 му-50  лу-50 су-100  d400
---------------------------------------------------------------------------------
-<esMyVyMyVy$>
-$organ: 50 му-50  ву-50 му-50  ву-50 су-50  ву-50 му-100 d400
-
-<esMyVyMyVy@>
+<simple44_drum@>
 -  : 1   2   3   4   :
 @hc: x x x x x x x x :
 @sn:     2       4   :
 @bd: 1       3       :
+--------------------------------------------------------------------------------
+<pg_оХаОО_ritm$>
+$organ: му-50 фу-50 фу-50 фу-50 му-50 фу-50 фу-50 фу-50
+--------------------------------------------------------------------------------
+<pg_ооЦаЦуЩаЩуПаХу_ritm$>
+$organ: му-50 му-50 ву-50 му-50 су-50 му-50 лу-50 зу-50
+--------------------------------------------------------------------------------
+<pg_ToEs_ritm$>
+$organ: му-50 му-50  ву-50 му-50  су-50 му-50  лу-50 су-100  d400
+--------------------------------------------------------------------------------
+<es_оЦаЦуЦаХаХуЦу_ritm$>
+$organ: 50 му-50  ву-50 му-50  ву-50 су-50  ву-50 му-100 d400
 --------------------------------------------------------------------------------
 <esMy6Fy$>
 $organ: 50 му-50  му-50 му-50  му-50 му-50  му-50 фу-50
@@ -166,30 +165,31 @@ $organ: му-50 му-50  му-50 му-50  му-50 му-50  му-50 су-100 d400
 <esMyFyOut$>
 $organ: му-50 му-50  му-50 му-50  му-50 му-50  му-50 су-100 d400
 --------------------------------------------------------------------------------
+# о
 <esMoKyLy$>
-$organ: 50 мо-100 ку-50  лу-100 му-50 му-100 d400
+$organ: 50 мо-100 ку-50  лу-100 му-50 му-150 d400
 
 <esMoKyLyLast$>
 $organ: 50 мо-100 ку-50  лу-100 му-50 су-100 d400
 --------------------------------------------------------------------------------
-<pg_00ЦаЦуЩа@>
+<pg_ооЦаЦуЩа_drum@>
 -  : 1   2   3   4   :
 @hc: x x x x x       :
 @sn:     2     x x x :
 @bd: 1       3       :
 
-<pg_00ЦаЦуЩа_ritm$>
-$organ: му-50 му-50 ву-50 му-50 су-50 150
+<pg_ооЦаЦуЩа_ritm$>
+$gdm: му-50 му-50 ву-50 му-50 су-50 150
 --------------------------------------------------------------------------------
-<pgBreakOnE1@>
+<pg_BreakOnE1_drum@>
 -  : 1   2   3   4   :
 @hc: x x x x x x x x :
 @sn: x         x x   :
 
-<pgBrakeOnE1_ritm$>
+<pg_BrakeOnE1_ritm$>
 $organ: му-50 200 му-50 му-50 50
 --------------------------------------------------------------------------------
-<pgBreakOnH@>
+<pg_BreakOnH_drum@>
 -  : 1   2   3   4   :
 @hc: x x x x x x x x :
 @sn:           x x   :
@@ -208,66 +208,68 @@ $organ: 200 му-50 му-50
 -  : 1   2   3   4   :
 @hc:                 :
 --------------------------------------------------------------------------------
-<hat@>
+<hat_drum@>
 -  : 1   2   3   4   :
 @hc: x   x   x   x   :
 --------------------------------------------------------------------------------
-<hat8@>
+<hat8_drum@>
 -  : 1   2   3   4   :
 @hc: x x x x x x x x :
 `.trim();
 
 const petGunBreak = `
-<out b120 r1>
+<out b110 r1>
 tick@
-simple44@-1 pg_00ЦаЦуЩаЩуПаХу_ritm$-1
-pg_00ЦаЦуЩа@ pg_00ЦаЦуЩа_ritm$
-pgBreakOnE1@ pgBrakeOnE1_ritm$
-pgBreakOnH@  pgBreakOnH_ritm$
-hat8@-1
-hat@-1 pg_оХаОО_ritm$-1
+simple44@-1        pg_ооЦаЦуЩаЩуПаХу_ritm$-1
+pg_ооЦаЦуЩа_drum@  pg_ооЦаЦуЩа_ritm$
+pg_BreakOnE1_drum@ pg_BrakeOnE1_ritm$
+pg_BreakOnH_drum@  pgBreakOnH_ritm$
+hat8_drum@-1
+hat_drum@-1        pg_оХаОО_ritm$-1
 pause@
 
-<out b120 r1>
+<out b110 r1>
 tick@
-simple44@-2 pg_00ЦаЦуЩаЩуПаХу_ritm$-2
-simple44@-1 pgToEs$-1
-esMyVyMyVy@-1 esMyVyMyVy$
-simple44@-2 pg_00ЦаЦуЩаЩуПаХу_ritm$-2
-simple44@-1 pgToEs$-1
-esMyVyMyVy@-1 esMyVyMyVy$
+simple44@-2 pg_ооЦаЦуЩаЩуПаХу_ritm$-2
+simple44@-1 pg_ToEs_ritm$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
+simple44@-2 pg_ооЦаЦуЩаЩуПаХу_ritm$-2
+simple44@-1 pg_ToEs_ritm$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 pause@
 
 ${parts}
 `;
 
+// pg_оХаОО
+// ооЦаЦуЩаЩуПаХу
+
 const full = `
-<out b120 r1>
+<out b110 r1>
 tick@
 
-hat@-2       pg_оХаОО_ritm$-2
-simple44@-2  pg_00ЦаЦуЩаЩуПаХу_ritm$-2
-simple44@-8  pg_00ЦаЦуЩаЩуПаХу_ritm$-8
-simple44@-7  pg_00ЦаЦуЩаЩуПаХу_ritm$-7
-pg_00ЦаЦуЩа@ pg_00ЦаЦуЩа_ritm$
-pgBreakOnE1@ pgBrakeOnE1_ritm$
-pgBreakOnH@  pgBreakOnH_ritm$
-hat8@-1
+hat_drum@-2        pg_оХаОО_ritm$-2
+simple44_drum@-2   pg_ооЦаЦуЩаЩуПаХу_ritm$-2
+simple44_drum@-8   pg_ооЦаЦуЩаЩуПаХу_ritm$-8
+simple44_drum@-7   pg_ооЦаЦуЩаЩуПаХу_ritm$-7
+pg_ооЦаЦуЩа_drum@  pg_ооЦаЦуЩа_ritm$
+pg_BreakOnE1_drum@ pg_BrakeOnE1_ritm$
+pg_BreakOnH_drum@  pgBreakOnH_ritm$
+hat8_drum@
 
-hat@-2 pg_оХаОО_ritm$-2
-simple44@-2 pg_00ЦаЦуЩаЩуПаХу_ritm$-2
-simple44@-1 pgToEs$-1
-esMyVyMyVy@-1 esMyVyMyVy$
-simple44@-2 pg_00ЦаЦуЩаЩуПаХу_ritm$-2
-simple44@-1 pgToEs$-1
-esMyVyMyVy@-1 esMyVyMyVy$
-
+hat_drum@-2       pg_оХаОО_ritm$-2
+simple44_drum@-2  pg_ооЦаЦуЩаЩуПаХу_ritm$-2
+simple44_drum@    pg_ToEs_ritm$
+simple44@         es_оЦаЦуЦаХаХуЦу_ritm$
+simple44_drum@-2  pg_ооЦаЦуЩаЩуПаХу_ritm$-2
+simple44_drum@    pg_ToEs_ritm$
+simple44@         es_оЦаЦуЦаХаХуЦу_ritm$
 
 ${parts}
 `;
 
 const beat44 = `
-<out b100 r1000>
+<out b110 r1000>
 simple44@
 
 <simple44@>
@@ -278,62 +280,62 @@ simple44@
 `.trim();
 
 const test = `
-<out b120 r1>
-simple44@-1 pgToEs$-1
-esMyVyMyVy@-1 esMyVyMyVy$
+<out b110 r1>
+simple44@-1 pg_ToEs_ritm$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLyLast$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMy6Fy$-1
 simple44@-1 esMyFy$-1
 simple44@-1 esMyFy2$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMy6Fy$-1
 simple44@-1 esMyFy$-1
 simple44@-1 esMyFy2$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLyLast$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLyLast$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 
 
-<out b120 r1>
-simple44@-1 pgToEs$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+<out b110 r1>
+simple44@-1 pg_ToEs_ritm$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMy6Fy$-1
 simple44@-1 esMyFy$-1
 simple44@-1 esMyFy2$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLy$-1
 simple44@-1 esMoKyLyLast$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
-<out b120 r1>
+<out b110 r1>
 tick@ 
 
 simple44@-2 esMyFy$-2
 simple44@-1 esMyFyOut$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 simple44@-2 esMyFy$-2
 simple44@-1 esMyFyOut$-1
-esMyVyMyVy@-1 esMyVyMyVy$-1
+simple44@-1 es_оЦаЦуЦаХаХуЦу_ritm$
 
-<out b120 r1>
+<out b110 r1>
 simple44@-2 esMoKyLy$-2
 
 ${parts}
@@ -342,9 +344,9 @@ ${parts}
 export default {
   content,
   tracks: [
-    { key: '4/4', value: beat44, name: 'бит 4/4 bmp:100' },
-    { key: 'break', value: petGunBreak, name: 'сбивка bmp:120' },
-    { key: 'full', value: full, name: 'полностью bmp:120' },
+    { key: '4/4', value: beat44, name: 'бит 4/4 bmp:110' },
+    { key: 'break', value: petGunBreak, name: 'сбивка bmp:110' },
+    { key: 'full', value: full, name: 'полностью bmp:110' },
     { key: 'test', value: test, name: 'тест' },
   ],
 };
