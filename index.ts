8402102e0099fe23249f393cd054a0636ad1f31c
blues74 <41178258+blues74@users.noreply.github.com>
2023-05-08 19:13:50 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -1,12 +1,3 @@
-
-let metaTag=document.createElement('meta');
-metaTag.name = "viewport"
-//<meta name="viewport" content="width=device-width, user-scalable=no">
-//metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-//metaTag.content = "width=device-width, initial-scale=1.0, user-scalable=no"
-metaTag.content = "width=device-width, user-scalable=no";
-document.getElementsByTagName('head')[0].appendChild(metaTag);
-
 import 'framework7-icons/css/framework7-icons.css';
 import 'framework7/framework7-bundle.min.css';
 import './src/style.css';
