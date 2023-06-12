3862351cf6b558fb8a74acda3b5fe992ff311eaf
Gurko Ilya <ext.igurko@tcsbank.ru>
2023-06-12 17:54:07 +0300

    drums

--- pages/drum-ctrl.ts
+++ pages/drum-ctrl.ts
@@ -20,8 +20,51 @@ interface Page {
 //     },
 // }
 
+type KeyData = {
+    code: string;
+    note: string;
+    down: number;
+    up: number;
+    next: number;
+};
+
+type BpmInfo = {
+    bpm: number;
+    lastDownTime: number;
+    pressCount: number;
+    totalMs: number;
+};
+
+const emptyBpmInfo = (): BpmInfo => {
+    //console.log('getEmptyBpm');
+    return {
+        bpm: 0,
+        lastDownTime: 0,
+        pressCount: 0,
+        totalMs: 0,
+    };
+};
+
 export class DrumCtrl {
+    bpmInfo: BpmInfo = emptyBpmInfo();
+    mode: 'record' | null = null;
+    keyData: KeyData | null = null;
+    keySequence: KeyData[] = [];
+    startUpTime = 0;
+    lastTickTime: number = 0;
+
     constructor(public page: Page) {}
+
+    clearRecordData() {
+        this.keyData = null;
+        this.keySequence = [];
+        this.startUpTime = 0;
+        this.lastTickTime = 0;
+    }
+
+    clearBpmInfo() {
+        this.bpmInfo = emptyBpmInfo();
+    }
 
     getContent(keyboardId: string): string {
         const topRowHeight = 5;
@@ -39,7 +82,7 @@ export class DrumCtrl {
             const label = key === info.noteLat ? key: `${key}:${info.noteLat}`;
 
             acc = acc + `
-                <a data-action-drum="${info.noteLat}" style="user-select: none;">${label}</a>&emsp;            
+                <a data-action-drum-key="${info.noteLat}" style="user-select: none;">${label}</a>&emsp;            
             `.trim();
 
             return acc;
@@ -61,14 +104,14 @@ export class DrumCtrl {
                         <div style="display: flex; width: 100%;">
                             <div 
                                 style="width: 50%; height: ${topRowHeight}rem; text-align: center; background-color: lightcyan; user-select: none; touch-action: none;"
-                                data-action-drum="cowbell"
+                                data-action-drum-key="cowbell"
                                 data-keyboard-id="${keyboardId}-${ind++}"                                
                             >
                                 <!--<br/>&nbsp;K-k-->
                             </div>
                             <div 
                                 style="width: 50%; height: ${topRowHeight}rem; text-align: center; background-color: lightyellow; user-select: none; touch-action: none;"
-                                data-action-drum="cowbell"
+                                data-action-drum-key="cowbell"
                                 data-keyboard-id="${keyboardId}-${ind++}"                                
                             >
                                 <!--<br/>&nbsp;T-t-->
@@ -85,7 +128,7 @@ export class DrumCtrl {
                             </div>
                             <div 
                                 style="width: 50%; height: ${midRowHeight/2}rem; box-sizing: border-box; text-align: center; background-color: whitesmoke; user-select: none; touch-action: none;"
-                                data-action-drum="hc"
+                                data-action-drum-key="hc"
                                 data-keyboard-id="${keyboardId}-${ind++}"
                                 data-highlight-drum="bd+hc"                                                                
                             >  
@@ -98,7 +141,7 @@ export class DrumCtrl {
                         >
                             <div 
                                 style="width: 50%; height: ${midRowHeight/2}rem; box-sizing: border-box; text-align: center; background-color: whitesmoke; user-select: none; touch-action: none;"
-                                data-action-drum="hc"
+                                data-action-drum-key="hc"
                                 data-keyboard-id="${keyboardId}-${ind++}"
                                 data-highlight-drum="sn+hc"                                
                             >       
@@ -115,9 +158,9 @@ export class DrumCtrl {
                         
                         <div
                             style="width: 100%; height: ${botRowHeight}rem; background-color: tan; user-select: none; touch-action: none;"
-                            data-action-drum="bd"  
+                            data-action-drum-key="bd"
                             data-keyboard-id="${keyboardId}-${ind++}"
-                            data-highlight-drum="bd+hc"                                          
+                            data-highlight-drum="bd+hc"
                         >
                             <!--&nbsp;O-o-->
                         </div>                    
@@ -126,30 +169,31 @@ export class DrumCtrl {
                     <div style="width: 33%; user-select: none; touch-action: none;">
                         <div
                             style="height: ${midRowHeight}rem; width: 100%; background-color: lightpink; user-select: none; touch-action: none;"
-                            data-action-drum="sn"
+                            data-action-drum-key="sn"
                             data-keyboard-id="${keyboardId}-${ind++}"
-                            data-highlight-drum="sn+hc"                                                        
+                            data-highlight-drum="sn+hc"                                                                                    
                         >
                             <!--&nbsp;V-v-->
                         </div>
                         
                         <div 
-                            style="width: 100%; height: ${botRowHeight}rem; text-align: center; user-select: none; touch-action: none;"
-                                data-action-drum="sn+hc"
-                                data-keyboard-id="${keyboardId}-${ind++}"                            
+                            style="width: 100%; height: ${botRowHeight}rem; text-align: center; user-select: none; touch-action: none;"                                                            
                         >
+                            <!--data-action-drum-key="sn+hc"
+                            data-keyboard-id="${keyboardId}-${ind++}"-->
                             <!--<br/>&nbsp;A-a-->
                         </div>                        
                         
                         <div 
                             style="width: 100%; height: ${botRowHeight}rem; text-align: center; user-select: none; touch-action: none;"
-                                data-action-drum="bd+hc"
-                                data-keyboard-id="${keyboardId}-${ind++}"
+                            data-action-drum="get-bpm-or-stop"
                         >
+                            <!--data-action-drum-key="bd+hc"
+                            data-keyboard-id="${keyboardId}-${ind++}"-->
                             <!--<br/>&nbsp;Q-q-->
                         </div>
                     </div>
-                        
+                                           
                     <div 
                         style="font-size: 1.7rem; font-family: monospace; height: 20rem; width: 100%; position: absolute; top: 0; pointer-events: none; user-select: none; touch-action: none; padding-left: .5rem;"
                         data-type="text-under-keyboard"
@@ -164,15 +208,26 @@ export class DrumCtrl {
                     </div>
                 </div>
                 
-                
+               
 
                 <div style="margin: .5rem; user-select: none; touch-action: none;">
-                    <span style="font-size: 1.5rem; user-select: none; touch-action: none;" data-action="clear">
+                    <span 
+                        style="font-size: 1.5rem; user-select: none; touch-action: none;"
+                        data-action-drum="clear"
+                    >
                         clr&nbsp;&nbsp;
                     </span>                
+                    <span 
+                        style="font-size: 1.5rem; user-select: none; touch-action: none;"
+                        data-action-drum="record"
+                    >
+                        rec&nbsp;&nbsp;
+                    </span>                    
                 </div>
+                
+                <div data-name="drum-record-out"></div>                
                                 
-                <div style="margin: .5rem; user-select: none; font-family: monospace; font-size: 2rem;">
+                <div style="font-size: 2rem; margin: .5rem; user-select: none; font-family: monospace; ">
                     O-k-T-k-O---V---<br/>                    
                     O-k-T-k-O---O-kt<br/>
                     O-ktK-v-O---O---<br/>
@@ -182,14 +237,14 @@ export class DrumCtrl {
                     O---T-k-O---O---<br/>                                                        
                 </div>
 
-                <div style="margin: .5rem; user-select: none; font-family: monospace; font-size: 2rem;">
+                <div style="font-size: 2rem; margin: .5rem; user-select: none; font-family: monospace;">
                     QoxoAoq_<br/>
                     X_qoA_xv<br/>
                     Q_q_Aoxo<br/>
                     X_x_A_xv
                 </div>
 
-                <div style="margin: .5rem; user-select: none; font-family: monospace; font-size: 2rem;">
+                <div style="font-size: 2rem; margin: .5rem; user-select: none; font-family: monospace;">
                     Q_q_Aoq_<br/>
                     X_q_A_xv<br/>
                     Q_q_Aoxo<br/>
--- pages/page_keyboard.ts
+++ pages/page_keyboard.ts
@@ -20,6 +20,9 @@ const synthesizer = new Synthesizer();
 synthesizer.connect({ ctx: Sound.ctx });
 synthesizer.setSettings(toneAndDrumPlayerSettings);
 
+const DOWN = 1;
+const UP = 0;
+
 type ViewType = 'bassSolo' | 'drums';
 
 const ns = {
@@ -39,8 +42,9 @@ interface Page {
 }
 
 export class KeyboardPage {
-    view: ViewType = 'bassSolo';
+    view: ViewType = 'drums'; // 'bassSolo';
     ctrl: BassSoloCtrl = new BassSoloCtrl();
+    drumCtrl: DrumCtrl;
 
     bpmValue = 100;
     playingTick = '';
@@ -170,10 +174,10 @@ export class KeyboardPage {
     }
 
     setDrumsContent() {
-        const ctrl = new DrumCtrl(this);
+        this.drumCtrl = new DrumCtrl(this);
 
         this.view = 'drums';
-        this.el$.html(ctrl.getContent('drums'));
+        this.el$.html(this.drumCtrl.getContent('drums'));
         this.bpmRange = (this.context.$f7 as any).range.create({
             el: dyName('slider', this.pageEl),
             on: {
@@ -316,21 +320,192 @@ export class KeyboardPage {
         });
     }
 
+    handleKeyRecord(code: string, time: number, type: 0 | 1) {
+        if (this.drumCtrl.mode !== 'record') {
+            return;
+        }
+
+        const ctrl = this.drumCtrl;
+
+        // ПЕРВОЕ НАЖАТИЕ
+        if (!ctrl.startUpTime) {
+            ctrl.startUpTime = ctrl.lastTickTime || time;
+        }
+
+        if (!ctrl.keyData && type === DOWN) {
+            ctrl.keyData = {
+                code,
+                down: time,
+                note: code,
+                up: 0,
+                next: 0,
+            };
+
+            return;
+        }
+
+        if (
+            ctrl.keyData &&
+            ((type === UP && code === ctrl.keyData.code) || type === DOWN)
+        ) {
+            if (!ctrl.keyData.up) {
+                ctrl.keyData.up = time;
+                //this.playSound(this.keyData.note, true);
+            }
+
+            if (type === DOWN) {
+                ctrl.keyData.next = time;
+                ctrl.keySequence.push(ctrl.keyData);
+
+                ctrl.keyData = {
+                    code,
+                    down: time,
+                    note: code,
+                    up: 0,
+                    next: 0,
+                };
+
+                //this.playSound(this.keyData.note);
+            }
+        }
+    }
+
+    getOut(bpmInfo: DrumCtrl['bpmInfo'], seq: DrumCtrl['keySequence'] ) {
+        let bpm = 0;
+        let qms = 0; // ms в четверти
+
+        if (bpmInfo.totalMs) {
+            qms = bpmInfo.totalMs / (bpmInfo.pressCount - 1);
+            bpm = Math.round(60000 / qms);
+        }
+
+        console.log(bpm, qms, this.drumCtrl.keySequence, this.drumCtrl.bpmInfo);
+
+        let quarterTime = bpmInfo.lastDownTime + qms;
+
+        let out = '';
+
+        seq.forEach((item, i) => {
+            let durationQ = Math.round(((item.next - item.down) / qms) * un.NUM_120);
+            let otklMs = item.down - (quarterTime + (qms * i));
+            let otklQ = Math.round((otklMs / qms) * un.NUM_120);
+            out = out + `${i} - ${otklMs} - ${otklQ} - ${durationQ}<br/>`
+            //console.log(i, item.down - (quarterTime + (qms * i)));
+        });
+
+        const el = dyName('drum-record-out', this.pageEl);
+        if (el) {
+            el.innerHTML = out;
+        }
+
+//  getOut2(multi: boolean = false): string {
+//     let bpm = 0;
+//     let qms = 0;
+//
+//     if (this.bpmInfo.totalMs) {
+//         qms = this.bpmInfo.totalMs / (this.bpmInfo.pressCount - 1);
+//         bpm = Math.round(60000 / qms);
+//     }
+//
+//     //console.log('this.bpmInfo', bpm, this.bpmInfo);
+//
+//     let textOut = '';
+//     let totalDuration = 0;
+//     const divider = multi ? '\n' : ' ';
+//     //const qms = 60000 / params.bpm / un.NUM_100;
+//
+//     this.keySequence.forEach((item, i) => {
+//         let durationQ = item.up - item.down;
+//         let pause = item.next - item.up;
+//
+//         durationQ = Math.round((durationQ / qms) * un.NUM_100);
+//         pause = Math.round((pause / qms) * un.NUM_100);
+//         // pause = Math.round(pause / 10);
+//
+//         // durationX = Math.round(duration / 10);
+//         // pause = Math.round(pause / 10);
+//
+//         const total = durationQ + pause;
+//         totalDuration += total;
+//         // sount:total:duration:pause
+//         //textOut += `${item.note}-${total}-${duration}-${pause}${divider}`;
+//         textOut += `${item.note}-${total}-${durationQ}-${pause}${divider}`;
+//     });
+//
+//     return `{b${bpm} d${totalDuration}${divider}${textOut}}`.trim();
+// }
+    }
+
     subscribeDrumsEvents() {
-        getWithDataAttr('action-drum', this.pageEl)?.forEach((el: HTMLElement) => {
-            const note1 = el.dataset['actionDrum'].split('+')[0];
+        getWithDataAttrValue('action-drum', 'get-bpm-or-stop', this.pageEl)?.forEach((el: HTMLElement) => {
+            el.addEventListener('pointerdown', (evt: MouseEvent) => {
+                // остановить запись
+                if (this.drumCtrl.keyData) {
+                    this.drumCtrl.keyData.next = Date.now();
+                    this.drumCtrl.keySequence.push(this.drumCtrl.keyData);
+                    this.getOut(this.drumCtrl.bpmInfo, this.drumCtrl.keySequence)
+                    this.drumCtrl.clearRecordData();
+
+                    return;
+                }
+
+                const bpmInfo = this.drumCtrl.bpmInfo;
+                const time = Date.now();
+
+                bpmInfo.pressCount++;
+
+                if (bpmInfo.lastDownTime) {
+                    bpmInfo.totalMs = bpmInfo.totalMs + (time - bpmInfo.lastDownTime);
+                }
+                bpmInfo.lastDownTime = time;
+
+                if (bpmInfo.totalMs) {
+                    const avg = bpmInfo.totalMs / (bpmInfo.pressCount - 1);
+                    bpmInfo.bpm = Math.round(60000 / avg);
+                }
+
+                el.innerText = '' + (bpmInfo.bpm || '');
+            });
+        });
+
+        getWithDataAttrValue('action-drum', 'clear', this.pageEl)?.forEach((el: HTMLElement) => {
+            el.addEventListener('pointerdown', (evt: MouseEvent) => {
+                getWithDataAttrValue('action-drum', 'get-bpm-or-stop', this.pageEl)?.forEach((el: HTMLElement) => {
+                    this.drumCtrl.clearBpmInfo();
+                    el.innerText = '';
+                });
+            });
+        });
+
+        getWithDataAttrValue('action-drum', 'record', this.pageEl)?.forEach((el: HTMLElement) => {
+            el.addEventListener('pointerdown', (evt: MouseEvent) => {
+                if (this.drumCtrl.mode !== 'record') {
+                    this.drumCtrl.mode = 'record';
+                    el.style.fontWeight = '700';
+                } else {
+                    this.drumCtrl.mode = null;
+                    el.style.fontWeight = '400';
+                }
+            });
+        });
+
+        getWithDataAttr('action-drum-key', this.pageEl)?.forEach((el: HTMLElement) => {
+            console.log(el, el.dataset['actionDrum']);
+            const note1 = (el.dataset['actionDrumKey'] || '').split('+')[0];
 
             if (!note1) {
                 return;
             }
 
-            const note2 = el.dataset['actionDrum'].split('+')[1];
+            const note2 = (el.dataset['actionDrum'] || '').split('+')[1];
             const notes = [note1, note2].filter(item => !!item);
 
             const volume = note1 === 'cowbell' ? 0.30 : undefined
             const keyboardId = el.dataset['keyboardId'];
 
             el.addEventListener('pointerdown', (evt: MouseEvent) => {
+                const time = Date.now();
+
                 notes.forEach(keyOrNote => {
                     synthesizer.playSound({
                         keyOrNote,
@@ -338,6 +513,8 @@ export class KeyboardPage {
                         id: keyboardId,
                         onlyStop: false,
                     });
+
+                    this.handleKeyRecord(keyOrNote, time, DOWN);
                 });
 
                 if (note2) {
@@ -348,12 +525,16 @@ export class KeyboardPage {
             });
 
             el.addEventListener('pointerup', (evt: MouseEvent) => {
+                const time = Date.now();
+
                 notes.forEach(keyOrNote => {
                     synthesizer.playSound({
                         keyOrNote,
                         id: keyboardId,
                         onlyStop: true,
                     });
+
+                    this.handleKeyRecord(keyOrNote, time, UP);
                 });
 
                 if (note2) {
