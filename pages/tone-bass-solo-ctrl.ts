b94a5e28f8a4d0d788dfca70a7ee650716ff2f13
Gurko Ilya <ext.igurko@tcsbank.ru>
2023-07-10 19:46:25 +0300

    tone

--- pages/tone-bass-solo-ctrl.ts
+++ pages/tone-bass-solo-ctrl.ts
@@ -1,4 +1,4 @@
-import {ToneCtrl} from './tone-ctrl';
+import { ToneCtrl } from './tone-ctrl';
 
 function getBgColor(note: string): string {
     note = note || '';
@@ -56,15 +56,14 @@ function getBgColor(note: string): string {
     return bgColor;
 }
 
-
 function getKeyFn(params: {
     id: string | number;
-    width?: string;
+    cellSize?: string;
     fontSize?: string;
 }): (note: string, symbol: string) => string {
     const id = params.id || '';
     const fontSize = params.fontSize || '1.3rem';
-    const width = params.width || '1.5rem';
+    const cellSize = params.cellSize || '1.5rem';
 
     return (note: string, symbol: string): string => {
         symbol = '&nbsp;';
@@ -85,10 +84,12 @@ function getKeyFn(params: {
         margin: 0;
         padding: 0;
         display: inline-block;
-        width: ${width};
+        width: ${cellSize};
+        height: ${cellSize};
         user-select: none;
         touch-action: none;
         font-size: ${fontSize};
+        line-height: ${fontSize};
         font-weight: ${fontWeight};
         text-align: center;
         color: ${fontColor};        
@@ -106,29 +107,49 @@ function getKeyFn(params: {
 } // getKeyFn
 
 
-function getKeyboardBass(id?: number | string): string {
-    const arr = [
-        ['za', 'la', 'ka', 'ba'],
-        ['ma', 'fa', 'va', 'sa'],
-        ['da', 'ta', 'ra', 'na'],
+const bassKeys: string[][] = [
+    ['za', 'la', 'ka', 'ba'],
+    ['ma', 'fa', 'va', 'sa'],
+    ['da', 'ta', 'ra', 'na'],
 
-        ['zo', 'lo', 'ko', 'bo'],
-        ['mo', 'fo', 'vo', 'so'],
-        ['do', 'to', 'ro', 'no'],
+    ['zo', 'lo', 'ko', 'bo'],
+    ['mo', 'fo', 'vo', 'so'],
+    ['do', 'to', 'ro', 'no'],
 
-        ['zy', 'ly', 'ky', 'by'],
-        ['my', 'fy', 'vy', 'sy'],
-        ['dy', 'ty', 'ry', 'ny'],
+    ['zy', 'ly', 'ky', 'by'],
+    ['my', 'fy', 'vy', 'sy'],
+    ['dy', 'ty', 'ry', 'ny'],
 
-        ['zu', 'lu', 'ku', 'bu'],
-        ['mu', 'fu', 'vu', 'su'],
-        ['du', 'tu', 'ru', 'nu'],
-    ];
+    ['zu', 'lu', 'ku', 'bu'],
+    ['mu', 'fu', 'vu', 'su'],
+    ['du', 'tu', 'ru', 'nu'],
+];
 
 
+const soloKeys: string[][] = [
+    ['ze', 'le', 'ke', 'be'],
+    ['me', 'fe', 've', 'se'],
+    ['de', 'te', 're', 'ne'],
+
+    ['za', 'la', 'ka', 'ba'],
+    ['ma', 'fa', 'va', 'sa'],
+    ['da', 'ta', 'ra', 'na'],
+
+    ['zo', 'lo', 'ko', 'bo'],
+    ['mo', 'fo', 'vo', 'so'],
+    ['do', 'to', 'ro', 'no'],
+
+    ['zy', 'ly', 'ky', 'by'],
+    ['my', 'fy', 'vy', 'sy'],
+    ['dy', 'ty', 'ry', 'ny'],
+];
+
+
+
+function getVerticalHarmKeyboard(id: number | string, keys: string[][]): string {
     id = id || '';
 
-    const getKey = getKeyFn({ id, width: '2rem', fontSize: '1.5rem' });
+    const getKey = getKeyFn({ id, cellSize: '1.9rem', fontSize: '1.5rem' });
 
     let keyboard = `
         <div style="
@@ -141,7 +162,7 @@ function getKeyboardBass(id?: number | string): string {
     `.trim();
 
     let tpl = '';
-    arr.forEach(row => {
+    keys.forEach(row => {
         tpl = tpl + '<div>';
 
         row.forEach(note => {
@@ -155,68 +176,14 @@ function getKeyboardBass(id?: number | string): string {
 
     return keyboard;
 }
-
-function getKeyboardSolo(id?: number | string): string {
-    const arr = [
-        ['ny', 'ry', 'ty', 'dy'],
-        ['sy', 'vy', 'fy', 'my'],
-        ['by', 'ky', 'ly', 'zy'],
-
-        ['no', 'ro', 'to', 'do'],
-        ['so', 'vo', 'fo', 'mo'],
-        ['bo', 'ko', 'lo', 'zo'],
-
-        ['na', 'ra', 'ta', 'da'],
-        ['sa', 'va', 'fa', 'ma'],
-        ['ba', 'ka', 'la', 'za'],
-
-        ['ne', 're', 'te', 'de'],
-        ['se', 've', 'fe', 'me'],
-        ['be', 'ke', 'le', 'ze'],
-    ];
-
-    id = id || '';
-
-    const getKey = getKeyFn({ id, width: '2rem', fontSize: '1.5rem' });
-
-    let keyboard = `
-        <div style="
-            font-family: monospace;
-            user-select: none;
-            touch-action: none;    
-            padding: 0.5rem 0 0.5rem 0.5rem;"
-            data-name="keyboard-${id}"
-        >%tpl%</div>
-    `.trim();
-
-    let tpl = '';
-    arr.forEach(row => {
-        tpl = tpl + '<div>';
-
-        row.forEach(note => {
-            tpl = tpl + getKey(note, note)
-        })
-
-        tpl = tpl + '</div>';
-    });
-
-    keyboard = keyboard.replace('%tpl%', tpl);
-
-    return keyboard;
-}
-
 
 export class BassSoloCtrl extends ToneCtrl {
     getContent(): string {
         let wrapper = `<div style="margin: .5rem; user-select: none; touch-action: none; display: flex; justify-content: space-between;">
-            ${getKeyboardBass('bass')}
-            ${getKeyboardSolo('solo')}
+            ${getVerticalHarmKeyboard('bass', bassKeys)}
+            ${getVerticalHarmKeyboard('solo', soloKeys)}
         </div>`.trim();
 
-        //wrapper =  wrapper.replace('%tpl%', getKeyboardBass('bass'));
-
         return wrapper;
-        // return keyboardSet.content;
     }
-
 }
