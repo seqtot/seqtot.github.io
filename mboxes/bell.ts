169d5e929bfee3f08f1e9698594a475d75ad4e58
Gurko Ilya <ext.igurko@tcsbank.ru>
2023-12-01 08:05:54 +0300

    long data

--- mboxes/bell.ts
+++ mboxes/bell.ts
@@ -1,9 +1,9 @@
 export default {
-  ns: 'band-song',
-  exportToLineModel: true,
-  pathNotesText: 'motes/bandit/bell.notes.midi',
-  pathNotesJson: 'motes/bandit/bell.notes.json',
-  content: `
+    ns: 'band-song',
+    exportToLineModel: true,
+    pathNotesText: 'motes/bandit/bell.midi',
+    pathNotesJson: 'motes/bandit/bell.json',
+    content: `
     <div style="margin: .5rem;">
         Bell
     </div>  
