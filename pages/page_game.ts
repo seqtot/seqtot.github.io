6d8cfd2e8730427a23c569841d5b1a4c8878e321
Gurko Ilya <ext.igurko@tcsbank.ru>
2024-03-18 17:38:52 +0300

    game board

--- pages/page_game.ts
+++ pages/page_game.ts
@@ -1,5 +1,5 @@
 import { RouteInfo } from '../src/router';
-import {getWithDataAttr} from '../src/utils';
+import { getWithDataAttr } from '../src/utils';
 
 type RouteType = {id: string, game: string}
 
@@ -18,7 +18,22 @@ type Sizes = {
 
     blockCount: number,
     rowInBlockCount: number;
+    cellCount: number;
+    cellWidth: number;
 };
+
+
+const colors = {
+    red: '255,0,0',
+    green: '0,255,0',
+    blue: '0,0,255',
+}
+
+const colors2 = {
+    black: '0,0,0',
+    white: '255,255,255',
+}
+
 
 function getSizes(x: {
     width: number,
@@ -27,6 +42,7 @@ function getSizes(x: {
     rowInBlockCount: number;
     paddingTop: number,
     paddingSide: number,
+    cellCount: number,
 }): Sizes {
     let pageWidth = Math.floor(x.width);
     let pageHeight = Math.floor(x.height);
@@ -34,6 +50,7 @@ function getSizes(x: {
     let boardHeight = pageHeight - x.paddingTop;
     let rowHeight = Math.floor((boardHeight / (x.rowInBlockCount * 2)) / 2) * 2;
     let halfRowHeight = rowHeight / 2;
+    let cellWidth = rowWidth / x.cellCount;
 
     const result =  {
         pageWidth,
@@ -46,11 +63,64 @@ function getSizes(x: {
         topPadding: 32,
         blockCount: x.blockCount,
         rowInBlockCount: x.rowInBlockCount,
+        cellCount: x.cellCount,
+        cellWidth,
     };
 
     console.log('getSizes', result);
 
     return result;
+}
+
+
+type ModelType = {
+    lines: {
+        restQ: number,
+        cells: {
+            offsetQ: number,
+            durQ: number,
+        }[]
+    }[]
+}
+
+
+const testBlock: ModelType = {
+    lines: [
+        {
+            restQ: 0,
+            cells: [{
+                offsetQ: 0,
+                durQ: 120,
+            }]
+        },
+        {
+            restQ: 0,
+            cells: [{
+                offsetQ: 30,
+                durQ: 90,
+            }]
+        },
+        {
+            restQ: 0,
+            cells: [{
+                offsetQ: 60,
+                durQ: 60,
+            }]
+        },
+        {
+            restQ: 0,
+            cells: [{
+                offsetQ: 90,
+                durQ: 30,
+            }]
+        }
+    ]
+};
+
+const model: {top: ModelType, mid: ModelType, bot: ModelType} = {
+    top: testBlock,
+    mid: testBlock,
+    bot: testBlock,
 }
 
 class Board {
@@ -83,10 +153,80 @@ class Board {
         this.canvasCtxMid.clearRect(0, 0, this.canvasMid.width, this.canvasMid.height);
     }
 
-    paint() {
-        this.clearCanvasMid();
+    paintCellGrid() {
+        const ctx = this.canvasCtxMid;
+        const defStroke = 'rgb(222, 222, 222)';
         const sizes = this.sizes;
 
+        ctx.strokeStyle = defStroke;
+        ctx.lineWidth = 1;
+        let lastY = 0;
+
+        const paintSection = (block: ModelType, lastY: number, rowHeight): number => {
+            block.lines.forEach((line, iLine) => {
+                ctx.beginPath();
+                ctx.moveTo(0, lastY);
+                ctx.lineTo(sizes.boardWidth, lastY);
+                ctx.stroke();
+
+                ctx.beginPath();
+                ctx.moveTo(0, lastY + rowHeight);
+                ctx.lineTo(sizes.boardWidth, lastY + rowHeight);
+                ctx.stroke();
+
+                for (let i = 0; i < this.sizes.cellCount; i++) {
+                    ctx.strokeRect(i * sizes.cellWidth, lastY, sizes.cellWidth, rowHeight);
+                }
+
+                lastY += rowHeight;
+            });
+
+            return lastY;
+        }
+
+        lastY = paintSection(model.top, lastY, this.sizes.halfRowHeight);
+        lastY = paintSection(model.mid, lastY, this.sizes.rowHeight);
+        lastY = paintSection(model.bot, lastY, this.sizes.halfRowHeight);
+    }
+
+    paintCellFill() {
+        const ctx = this.canvasCtxMid;
+        const defStroke = 'rgb(222, 222, 222)';
+        const sizes = this.sizes;
+        const w = sizes.boardWidth / 120;
+
+        ctx.strokeStyle = defStroke;
+        ctx.lineWidth = 1;
+        let lastY = 0;
+
+        const paintSection = (block: ModelType, lastY: number, rowHeight): number => {
+            block.lines.forEach((line, iLine) => {
+                line.cells.forEach((cell, iCell) => {
+                    const startX = cell.offsetQ * w;
+                    const endX = (cell.offsetQ + cell.durQ) * w;
+
+                    ctx.fillStyle = `rgba(${colors.blue}, 1)`;
+                    ctx.fillRect(startX, lastY, endX - startX, rowHeight);
+                    ctx.fillStyle = `rgba(${colors2.black}, .7)`;
+                    ctx.fillRect(startX + 1, lastY + 1, 4 , rowHeight - 2);
+                });
+
+                lastY += rowHeight;
+            });
+
+            return lastY;
+        }
+
+        lastY = paintSection(model.top, lastY, this.sizes.halfRowHeight);
+        lastY = paintSection(model.mid, lastY, this.sizes.rowHeight);
+        lastY = paintSection(model.bot, lastY, this.sizes.halfRowHeight);
+    }
+
+
+    paint() {
+        this.clearCanvasMid();
+
+        const sizes = this.sizes;
         const ctx = this.canvasCtxMid;
         const defStroke = 'rgb(200, 200, 200)';
         ctx.strokeStyle = defStroke;
@@ -103,34 +243,49 @@ class Board {
         lastY = lastY + height;
         height = sizes.rowInBlockCount * sizes.halfRowHeight;
         ctx.strokeRect(0, 0, sizes.boardWidth, lastY + height);
+
+        const w = sizes.boardWidth / 120;
+
+        this.paintCellFill();
+        this.paintCellGrid();
+
+        ctx.fillStyle = `rgba(${colors2.black}, .4)`;
+
+        lastY = 0;
+        height = sizes.rowInBlockCount * sizes.halfRowHeight;
+        ctx.fillRect(0, lastY, sizes.boardWidth , height);
+
+        lastY = sizes.boardHeight - (sizes.rowInBlockCount * sizes.halfRowHeight);
+        ctx.fillRect(0, lastY, sizes.boardWidth , height);
     }
 
     createCanvas(sizes: Sizes, canvasEl: HTMLElement) {
         this.sizes = sizes;
         this.canvasEl = canvasEl;
 
+        function setStyle(el: HTMLCanvasElement) {
+            el.style.position = "absolute";
+            el.style.userSelect = "none";
+            el.style.touchAction = "none";
+            el.width  = sizes.boardWidth;
+            el.height = sizes.boardHeight;
+        }
+
         // BOT TOM
         this.canvasBot = document.createElement('canvas');
-        this.canvasBot.style.position = "absolute";
-        this.canvasBot.width  = sizes.boardWidth;
-        this.canvasBot.height = sizes.boardHeight;
-        // this.canvasEl.style.border   = "1px solid gray";
+        setStyle(this.canvasBot);
         this.canvasEl.appendChild(this.canvasBot);
         this.canvasCtxBot = this.canvasBot.getContext("2d");
 
         // MID CANVAS
         this.canvasMid = document.createElement('canvas');
-        this.canvasMid.style.position = "absolute";
-        this.canvasMid.width  = sizes.boardWidth;
-        this.canvasMid.height = sizes.boardHeight;
+        setStyle(this.canvasMid);
         this.canvasEl.appendChild(this.canvasMid);
         this.canvasCtxMid = this.canvasMid.getContext("2d");
 
         // TOP CANVAS
         this.canvasTop = document.createElement('canvas');
-        this.canvasTop.style.position = "absolute";
-        this.canvasTop.width  = sizes.boardWidth;
-        this.canvasTop.height = sizes.boardHeight;
+        setStyle(this.canvasTop);
         this.canvasEl.appendChild(this.canvasTop);
         this.canvasCtxTop = this.canvasTop.getContext("2d");
     }
@@ -182,18 +337,25 @@ export class GamePage {
             rowInBlockCount: 4,
             paddingTop: 48,
             paddingSide: 8,
+            cellCount: 4,
         })
 
         console.log('sizes', this.sizes);
 
         this.pageEl.innerHTML = `
             <div>
-                <div style="padding: 8px;">
+                <div style="padding: 8px 8px 0 8px;">
                     <button data-start-game-action>start</button>
                     <button data-stop-game-action>stop</button>                    
                 </div>
                 <div
-                    style="width: ${sizes.boardWidth}px; height: ${sizes.boardHeight}px; padding-left: 8px;" 
+                    style="
+                        width: ${sizes.boardWidth}px;
+                        height: ${sizes.boardHeight}px;
+                        padding: 8px 0 8px 8px;
+                        user-select: none;
+                        touch-action: none;
+                    " 
                     data-game-board
                 >
                 </div>
